c2ed248f318e95c4dd95ba630112ee5ff1a0ea82
Allen Boynton <aboynton@fullsail.edu>
2016-06-29 17:12:17 -0400

    Initial setup
    Set up foundation and frameworks needed thus far. AV and
    Multipeer-connectivity.

---
+++ PoolBuddy/PoolBuddy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PoolBuddy
+//
+//  Created by Allen Boynton on 6/29/16.
+//  Copyright © 2016 Full Sail. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MultipeerConnectivity
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
